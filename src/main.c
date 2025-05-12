6fa2e0ffcf49e87138dc0c3c51b4018e1ab63118
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2025-05-12 14:57:11 -0300

    hello world

--- src/main.c
+++ src/main.c
@@ -1,7 +1,18 @@
-// #include "std/stdio.h"
+#include "lib/std/stdio.h"
 
 int main()
 {
-    _outchar('a', 0);
+    _outchar('X', 0);
+    _outchar('U', 1);
+    _outchar('P', 2);
+    _outchar('A', 3);
+    _outchar(' ', 4);
+    _outchar('F', 5);
+    _outchar('E', 6);
+    _outchar('D', 7);
+    _outchar('E', 8);
+    _outchar('R', 9);
+    _outchar('A', 10);
+    _outchar('L', 11);
     return 0;
 }
