c12a27a7ecb1f23f46dd5c15ffbe77579120eb01
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2025-05-13 14:24:09 -0300

    Reconfigura lib std

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
-test:
-	./bin/compilador -i ./src/main.c -o ./build/main.asm
+test: clear
+	cat ./static/stdio.asm >> ./build/main.asm
+	cat ./static/string.asm >> ./build/main.asm
+
+	./bin/compilador -i ./src/main.c >> ./build/main.asm
 	./bin/montador ./build/main.asm ./build/main.mif
 	./bin/simulador build/main.mif charmap.mif
 
--- src/main.c
+++ src/main.c
@@ -1,18 +1,7 @@
-#include "lib/std/stdio.h"
+#include "lib/stdio.h"
 
 int main()
 {
-    _outchar('X', 0);
-    _outchar('U', 1);
-    _outchar('P', 2);
-    _outchar('A', 3);
-    _outchar(' ', 4);
-    _outchar('F', 5);
-    _outchar('E', 6);
-    _outchar('D', 7);
-    _outchar('E', 8);
-    _outchar('R', 9);
-    _outchar('A', 10);
-    _outchar('L', 11);
+    _outchar("A", 0);
     return 0;
 }
